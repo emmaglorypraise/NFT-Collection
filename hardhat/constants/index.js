443fffc3cf99177f5cdf704634a9c8bf51088bc6
emmaglorypraise <emmaglorypraise@gmail.com>
2022-04-09 00:06:54 +0100

    add files

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xDD44f9AaEe867a81f43484bC85dD8DD22eF35EB3";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
